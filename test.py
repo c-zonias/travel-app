edb7c48dd5a8f30a053745b5bb500a395838f39e
Christos Zonias <christos.zonias9@gmail.com>
2024-11-16 01:37:44 +0200

    testing the api

---
+++ test.py
@@ -0,0 +1,22 @@
+from amadeus import Client, ResponseError
+
+# Replace with your API key and secret
+#API_KEY = "H3iIn3ADdf0UxsLWeqVWhHu7XuISK5kZ"
+#API_SECRET = "jQJIEIxIwWarKF4G"
+
+# Initialize the Amadeus client
+amadeus = Client(
+    client_id="H3iIn3ADdf0UxsLWeqVWhHu7XuISK5kZ",
+    client_secret="jQJIEIxIwWarKF4G"
+)
+
+try:
+    response = amadeus.shopping.flight_offers_search.get(
+        originLocationCode='MAD',
+        destinationLocationCode='ATH',
+        departureDate='2024-12-01',
+        adults=1
+    )
+    print(response.data)
+except ResponseError as error:
+    print(f"An error occurred: {error}")
